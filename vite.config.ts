97399156f71991448ffd1fb874a4105e34695dbd
Hassan-Sabbir <sabbirhassan7921@gmail.com>
2025-02-19 16:19:48 +0600

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
 import react from '@vitejs/plugin-react';
+
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-    tsconfigPaths(),
-    react()
-  ],
+	plugins: [
+		tailwindcss(),
+		tsconfigPaths(),
+		react()
+	]
 });
