ae875a1d337c4cf96f39dee87787537320c17ee8
Hassan-Sabbir <sabbirhassan7921@gmail.com>
2025-02-24 18:03:03 +0600

    commit

--- src/hooks/useApi.ts
+++ src/hooks/useApi.ts
@@ -1,15 +1,66 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, UseMutationResult, useQuery, useQueryClient } from "@tanstack/react-query";
 import { deleteData, getData, patchData, postData } from "../services/apiService";
+import { showErrorToast, showSuccessToast } from "../utils/toastUtils";
+
+interface ApiResponse<T> {
+    statusCode: number;
+    data: T;
+    total?: number;
+    pageCount?: number;
+}
+
+interface ErrorResponse {
+    error: boolean;
+    message: string;
+}
 
 export interface MutationProps {
     url: string;
     token?: string | null;
-    body?: Record<string, unknown>; 
+    body?: Record<string, unknown>;
     status?: number;
+}
+
+interface HandleMutationProps<T> {
+    mutation: UseMutationResult<T, any, { url: string; body?: any }>;
+    url: string;
+    body?: any;
+    invalidateQueryKey?: string[];
+    showSuccessMessage?: boolean;
+    showErrorMessage?: boolean;
+    requiredFields?: Array<{ key: string; label?: string }>;
+}
+
+const isSuccessfulResponse = (response: any): boolean => {
+    return response?.token || response?.statusCode === 200 || response?.statusCode === 201;
+};
+
+const handleErrorMessage = (error: any) => {
+    const errorMessage = error?.response?.data?.message || error?.data?.message || error?.message;
+    if (errorMessage) {
+        Array.isArray(errorMessage) ? errorMessage.forEach(showErrorToast) : showErrorToast(errorMessage);
+    }
+};
+
+interface GetResponseProps {
+    queryKey: string[];
+    url: string;
+    enabled?: boolean;
+    refetchOnWindowFocus?: boolean;
+    refetchOnMount?: boolean;
+    refetchInterval?: number;
+    showToast?: boolean;
+    staleTime?: number;
+}
+
+export interface ValidationProps {
+    body: Record<string, any>;
+    requiredFields: Array<{ key: string; label?: string }>;
 }
 
 export const useAPI = () => {
     const token = '';
+    const queryClient = useQueryClient();
 
     const getMutation = useMutation({
         mutationFn: ({ url }: MutationProps) => getData({ url, token })
@@ -27,10 +78,122 @@ export const useAPI = () => {
         mutationFn: ({ url, body }: MutationProps) => patchData({ url, body: (body as Record<string, any>), token })
     })
 
+    const usePaginatedQuery = ({
+        queryKey,
+        url,
+        enabled = true,
+        refetchOnWindowFocus = false,
+        refetchOnMount = false,
+        refetchInterval,
+        showToast = true,
+        staleTime = 0
+    }: GetResponseProps) => {
+        const {
+            data: response,
+            error,
+            isLoading,
+            isFetching,
+            ...queryProps
+        } = useQuery<ApiResponse<any> | ErrorResponse, Error, ApiResponse<any>>({
+            queryKey,
+            queryFn: () => getData({ url, token }),
+            refetchOnWindowFocus,
+            refetchOnMount,
+            staleTime,
+            refetchInterval,
+            enabled
+        });
+
+        let data: any = [];
+        let totalItems = 0;
+        let pageCount = 1;
+
+        if (response?.statusCode === 200) {
+            const { data: apiData, total, pageCount: apiPageCount } = response;
+
+            if (Array.isArray(apiData)) {
+                data = apiData;
+                totalItems = total ?? apiData.length;
+                pageCount = apiPageCount ?? 1;
+            } else if (apiData && Array.isArray(apiData.data)) {
+                data = apiData.data;
+                totalItems = apiData.total ?? 0;
+                pageCount = apiData.pageCount ?? 1;
+            } else {
+                data = apiData;
+            }
+        } else if (error) {
+            const errorMessage = (error as any)?.response?.data?.message ?? error?.message;
+
+            if (showToast) {
+                showErrorToast(errorMessage);
+            } else {
+                console.error(errorMessage);
+            }
+        }
+
+        return { data, totalItems, pageCount, response: response?.data, isLoading, isFetching, ...queryProps };
+    };
+
+    const validateFormData = ({
+        body,
+        requiredFields
+    }: ValidationProps): boolean => {
+        if (requiredFields?.length > 0) {
+            for (const { key: fieldKey, label: fieldLabel } of requiredFields) {
+                const fieldName = fieldLabel || fieldKey;
+                const fieldValue = body[fieldKey];
+
+                if (!fieldValue || fieldValue?.length == 0) {
+                    const action = fieldKey === "file" ? "upload" : "enter";
+                    showErrorToast(`Please ${action} ${fieldName}.`);
+                    return false;
+                }
+            }
+        }
+        return true;
+    };
+
+    const handleApiMutation = async ({
+        mutation,
+        url,
+        body = {},
+        invalidateQueryKey = [],
+        showSuccessMessage = true,
+        showErrorMessage = true,
+        requiredFields = []
+    }: HandleMutationProps<any>) => {
+        let isLoading = true;
+        try {
+            if (validateFormData({ body, requiredFields })) {
+                const response = await mutation.mutateAsync({ url, body });
+                isLoading = false;
+        
+                if (isSuccessfulResponse(response)) {
+                    if (showSuccessMessage) showSuccessToast(response?.message);
+                    if (invalidateQueryKey.length > 0) {
+                        await queryClient.invalidateQueries({ queryKey: invalidateQueryKey });
+                    }
+                    return { success: true, data: response, isLoading: false };
+                } else if (response?.message && showErrorMessage) {
+                    handleErrorMessage(response);
+                }
+            }
+        } catch (e: any) {
+            isLoading = false;
+            if (showErrorMessage) handleErrorMessage(e);
+            return { success: false, error: e, isLoading: false };
+        }
+    
+        return { success: false, isLoading: false };
+    };
+
     return {
         getMutation,
         deleteMutation,
         postMutation,
-        patchMutation
+        patchMutation,
+        usePaginatedQuery,
+        handleApiMutation
     };
 }
